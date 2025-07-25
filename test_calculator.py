48c6225307596439d7c764352bacf37d38b34ccb
sumanthskc <sumanthkumar.c@datagrokr.co>
2025-07-25 11:48:54 +0530

    Added content

--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ from calculator import add, subtract, multiply
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2,3),5)
+        self.assertEqual(add(3,3),6)
     def test_subtract(self):
         self.assertEqual(subtract(5,2),3)
     def test_add(self):
