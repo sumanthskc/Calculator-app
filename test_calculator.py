f4ab38bc6f797e24d908e3bb7e85ccef2f423ea7
sumanthskc <sumanthkumar.c@datagrokr.co>
2025-07-29 12:52:24 +0530

    trigger

--- test_calculator.py
+++ test_calculator.py
@@ -7,7 +7,7 @@ class TestCalculator(unittest.TestCase):
     def test_subtract(self):
         self.assertEqual(subtract(5,2),3)
     def test_add(self):
-        self.assertEqual(multiply(2,3),6)
+        self.assertEqual(multiply(3,3),9)
 
 if __name__=='__main__':
     unittest.main()
